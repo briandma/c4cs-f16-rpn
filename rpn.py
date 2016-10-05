a60850e66be6136ebdf25aee4122e4afc1dbeefe
Brian <briandma@umich.edu>
2016-10-05 14:57:07 -0400

    refactor

--- rpn.py
+++ rpn.py
@@ -1,35 +1,28 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv
+}
+
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
-        if token == '+':
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
-            result = arg1 + arg2
+            result = function(arg1, arg2)
             stack.append(result)
-        elif token == '-':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 - arg2
-            stack.append(result)
-        elif token == '*':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 * arg2
-            stack.append(result)
-        elif token == '/':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            if arg2 == 0:
-                result = float("inf")
-            else:
-                result = arg1 / arg2
-            stack.append(result)
-        else:
-            stack.append(int(token))
-        if len(stack) != 1:
-            raise TypeError("Too many parameters")
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():

3fa0cd1c86b32a77a32f46807640e4af505a7e8d
Brian <briandma@umich.edu>
2016-10-05 14:26:51 -0400

    subtract

--- rpn.py
+++ rpn.py
@@ -4,12 +4,19 @@ def calculate(myarg):
     stack = list()
     for token in myarg.split():
         if token == '+':
-            arg1 = stack.pop()
             arg2 = stack.pop()
+            arg1 = stack.pop()
             result = arg1 + arg2
+            stack.append(result)
+
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
             stack.append(result)
         else:
             stack.append(int(token))
+        
     return stack.pop()
 
 def main():
